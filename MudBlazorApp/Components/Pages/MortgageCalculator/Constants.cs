5ae6881df9254ac74d05a11a0f9b3594bb78c4ae
avo <avo@DESKTOP-EHQAQA8>
2026-08-27 20:20:51 -0400

    -

--- MudBlazorApp/Components/Pages/MortgageCalculator/Constants.cs
+++ MudBlazorApp/Components/Pages/MortgageCalculator/Constants.cs
@@ -5,8 +5,8 @@ public static class Constants
 
 	public static Dictionary<string, MortgageSettings.InterestRateTypes> InterestRateType = new()
 	{
-		{ "Fixed Rate", MortgageSettings.InterestRateTypes.Fixed },
-		{ "Adjustable Rate Mortgage (ARM)", MortgageSettings.InterestRateTypes.ARM },
+		{ "Fixed", MortgageSettings.InterestRateTypes.Fixed },
+		{ "ARM (Adjustable Rate Mortgage)", MortgageSettings.InterestRateTypes.ARM },
 	};
 
 	public static Dictionary<string, int> CompoundPeriod = new()
